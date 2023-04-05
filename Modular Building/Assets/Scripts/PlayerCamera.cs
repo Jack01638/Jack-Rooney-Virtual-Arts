11f9abe443f7afa193bfabbf320a03028ac02214
Jack01638 <79591906+Jack01638@users.noreply.github.com>
2023-04-06 00:28:02 +0100

    Movements Added
    -fixed movement of player
    -camera follows mouse movements
    -easier access of UI elements with pausing of camera rotation
    -UI updates

--- Modular Building/Assets/Scripts/PlayerCamera.cs
+++ Modular Building/Assets/Scripts/PlayerCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCamera : MonoBehaviour
 {
@@ -8,29 +9,56 @@ public class PlayerCamera : MonoBehaviour
     public float sensY;
     public Transform orientation;
 
+    private bool escPressed = false;
+
     float xRotation;
     float yRotation;
     // Start is called before the first frame update
     private void Start()
     {
-        //Cursor.lockState = CursorLockMove.Locked;
-        //Cursor.Visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        //in update, set this to unlocked and visable when esc toggled to access UI
     }
 
     // Update is called once per frame
     private void Update()
     {
-        //rotating camera with cursor
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        if (escPressed == false)
+        {
+            //rotating camera with cursor
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-        //make rotation changes
-        yRotation += mouseX;
-        xRotation -= mouseY;
+            //make rotation changes
+            yRotation += mouseX;
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        //
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+            //
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //not been pressed yet, game active and mouse locked
+            if (escPressed == false)
+            {
+                Debug.Log("Menu Open");
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                escPressed = true;
+            }
+            //mouse unlocked then:
+            else if (escPressed == true)
+            {
+                Debug.Log("Menu Closed");
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                escPressed = false;
+            }
 
+        }
     }
 }
--- Modular Building/Assets/Scripts/PlayerMovement.cs
+++ Modular Building/Assets/Scripts/PlayerMovement.cs
@@ -4,5 +4,108 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed;
+    public float playerHeight;
+    public LayerMask groundCheck;
+    bool grounded;
+    public float groundDrag;
 
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    public KeyCode jumpkey = KeyCode.Space;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        readyToJump = true;
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundCheck);
+
+        MyInput();
+        SpeedControl();
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(jumpkey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //max speed reached
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 maxVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(maxVel.x, rb.velocity.y, maxVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        //reset jump velocity so jump is consistent
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
 }
